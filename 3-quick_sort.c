bcade9843a9c5bb09b20607925f95f1a1f12c35e
Toby58U <tobennaumeh001@gmail.com>
2024-01-22 14:16:48 -0800

    Sorting algorithms in action

---
+++ 3-quick_sort.c
@@ -0,0 +1,90 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * lomuto_partition - Order a subset of an array of integers according to
+ *                    the lomuto partition scheme (last element as pivot).
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ *
+ * Return: The final partition index.
+ */
+int lomuto_partition(int *array, size_t size, int left, int right)
+{
+	int *pivot, higher, lower;
+
+	pivot = array + right;
+	for (higher = lower = left; lower < right; lower++)
+	{
+		if (array[lower] < *pivot)
+		{
+			if (lower > higher)
+			{
+				swap(array + lower, array + higher);
+				print_array(array, size);
+			}
+			higher++;
+		}
+	}
+
+	if (array[higher] > *pivot)
+	{
+		swap(array + higher, pivot);
+		print_array(array, size);
+	}
+
+	return (higher);
+}
+
+/**
+ * lomuto_sort_recursive - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+void lomuto_sort_recursive(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto_partition(array, size, left, right);
+		lomuto_sort_recursive(array, size, left, part - 1);
+		lomuto_sort_recursive(array, size, part + 1, right);
+	}
+}
+
+/**
+ * quick_sort - Sort an array of integers in ascending
+ *              order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Lomuto partition scheme. Prints
+ *              the array after each swap of two elements.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort_recursive(array, size, 0, size - 1);
+}
